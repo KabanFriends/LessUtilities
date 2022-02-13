61bd0780720356026026d8edbbc139a5f9664506
KabanFriends <yuukun2004@protonmail.ch>
2022-02-13 19:43:31 +0900

    Fix item utils

--- src/main/java/io/github/kabanfriends/lessutilities/utils/ItemUtils.java
+++ src/main/java/io/github/kabanfriends/lessutilities/utils/ItemUtils.java
@@ -25,18 +25,11 @@ public class ItemUtils {
             ItemStack i = mainInventory.get(index);
             ItemStack compareItem = i.copy();
             compareItem.setCount(item.getCount());
-            if (item == compareItem) {
-                while (i.getCount() < i.getMaxStackSize() && item.getCount() > 0) {
-                    i.setCount(i.getCount() + 1);
-                    item.setCount(item.getCount() - 1);
-                }
-            } else {
-                if (i.getItem() == Items.AIR) {
-                    if (index < 9)
-                        LessUtilities.MC.gameMode.handleCreativeModeItemAdd(item, index + 36);
-                    mainInventory.set(index, item);
-                    return;
-                }
+            if (i.getItem() == Items.AIR) {
+                if (index < 9)
+                    LessUtilities.MC.gameMode.handleCreativeModeItemAdd(item, index + 36);
+                mainInventory.set(index, item);
+                return;
             }
         }
     }
